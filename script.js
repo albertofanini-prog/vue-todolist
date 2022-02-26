581e23472f67a8993e2e0ac55ed66e546aab4f17
albertofanini-prog <albertofanini@outlook.com>
2022-02-26 14:59:57 +0100

    2nd Commit - List visibile, input and button

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('Hello Vue!');
+
+//Stampare all'interno di una lista un item per ogni todo
+    //Se è true visualizzare il testo del todo sbarrato
+//Visualizzare a fianco ad ogni item una "X". Cliccando su di essa il ToDo verrà rimosso.
+//Predisporre un campo di input testuale e un pulsante "aggiungi"
+    //Cliccando sudi esso il testo digitato viene usato per creare un nuovo ToDo.
+
+const app = new Vue ({
+    el: '#app',
+    data:{
+        items:[
+            'mela',
+            'pera',
+        ],
+    },
+    methods:{
+        addItem(){
+            this.toDo.push(this.newToDo);
+            this.newToDo="";
+        },
+    },
+})
